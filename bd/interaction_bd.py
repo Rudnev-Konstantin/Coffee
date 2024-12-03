7f25fc5eb450342a6f51d86e05e587c5cafee3fd
Rudnev Konstantin <konstrudserg.030408@gmail.com>
2024-12-03 23:30:32 +0300

    Исправил мелкие баги, доделал диалоговое окно - теперь оно взаимодействует с бд, добавил функционал в interaction_bd, а именно изменение данных

--- bd/interaction_bd.py
+++ bd/interaction_bd.py
@@ -13,8 +13,31 @@ def add_data(variety_name="Variety Name", roast_level="Medium", grind_type="Grou
         INSERT INTO 
         coffee(variety_name, roast_level, grind_type, taste_description, price, package_volume)
         VALUES
-        ({variety_name}, {roast_level}, {grind_type}, {taste_description}, {price}, {package_volume})
+        ("{variety_name}", "{roast_level}", "{grind_type}", "{taste_description}",
+        {price}, "{package_volume}")
         """)
+
+
+# функция для изменения записи
+def edit_data(id_title, variety_name="Variety Name", roast_level="Medium", grind_type="Ground",
+              taste_description="description...", price=0, package_volume="500g"):
+    with bd.connect("bd/coffee.sqlite") as con:
+        cur = con.cursor()
+        
+        try:
+            id_title = int(id_title)
+        except ValueError:
+            pass
+        
+        cur.execute(f"""
+        UPDATE coffee
+        SET roast_level = "{roast_level}", grind_type = "{grind_type}",
+            taste_description = "{taste_description}", variety_name = "{variety_name}",
+            price = {price}, package_volume = "{package_volume}"
+        WHERE id = ? OR variety_name = ?
+        """, (id_title, id_title))
+        
+        print("Вроде хотяб без ишибки", id_title)
 
 
 # функция для добавления шаблонных записей в таблицу
@@ -48,9 +71,9 @@ def data_acquisition(id_title=None):
             except ValueError:
                 pass
             
-            result = cur.execute(f'''
+            result = cur.execute('''
             SELECT * FROM coffee
-            WHERE id = {id_title} OR variety_name = "{id_title}"
-            ''').fetchone()
+            WHERE id = ? OR variety_name = ?
+            ''', (id_title, id_title)).fetchone()
         
         return result
--- components/add_edit_coffee/add_edit_coffee_form.py
+++ components/add_edit_coffee/add_edit_coffee_form.py
@@ -1,24 +1,32 @@
 from PyQt6 import uic
-from PyQt6.QtWidgets import QApplication, QDialog, QMessageBox
-import sys
+from PyQt6.QtWidgets import QDialog, QMessageBox, QAbstractButton, QDialogButtonBox
+from PyQt6.QtCore import pyqtSlot
 
-from bd.interaction_bd import add_data, data_acquisition
+from bd.interaction_bd import add_data, edit_data, data_acquisition
 
 
 class AddEditForm(QDialog):
     def __init__(self):
         super().__init__()
         uic.loadUi("components/add_edit_coffee/addEditCoffeeForm.ui", self)
-        self.id_title_LineEdit.setFocus()
         
-        self.add_edit_CheckBox.stateChanged.connect(self.switching_modes)
         self.switching_modes()
         
+        self.add_edit_CheckBox.stateChanged.connect(self.switching_modes)
+        
         self.find_PushButton.clicked.connect(self.search_coffee)
+        
+        self.buttonBox.clicked.connect(self.on_button_clicked)
+    
+    @pyqtSlot(QAbstractButton)
+    def on_button_clicked(self, button):
+        if self.buttonBox.buttonRole(button) == QDialogButtonBox.ButtonRole.AcceptRole:
+            self.add_edit_coffee()
     
     def switching_modes(self):
         if self.add_edit_CheckBox.isChecked():
             self.find_PushButton.hide()
+            self.id_title_LineEdit.hide()
             self.add_edit_CheckBox.setText("Добавить")
             
             self.variety_name.setText("название сорта")
@@ -28,12 +36,16 @@ class AddEditForm(QDialog):
             self.price.setText("цена")
             self.package_volume.setText("объем упаковки")
             
-            self.id_title_LineEdit.setText("id / Название")
+            self.id_title_LineEdit.setFocus()
         else:
             self.find_PushButton.show()
+            self.id_title_LineEdit.show()
+            
             self.add_edit_CheckBox.setText("Изменить")
             
             self.id_title_LineEdit.setText("id / Название")
+            
+            self.id_title_LineEdit.setFocus()
     
     def search_coffee(self):
         coffee_data = data_acquisition(self.id_title_LineEdit.text())
@@ -47,12 +59,18 @@ class AddEditForm(QDialog):
                           self.taste_description, self.price, self.package_volume]
         for i, coffee_item in enumerate(coffee_data[1:]):
             data_line_edit[i].setText(str(coffee_item))
-
-
-if __name__ == '__main__':
-    app = QApplication(sys.argv)
     
-    window = AddEditForm()
-    window.show()
-    
-    sys.exit(app.exec())
+    def add_edit_coffee(self):
+        data_item_coffee = (
+            self.variety_name.text(),
+            self.roast_level.text(),
+            self.grind_type.text(),
+            self.taste_description.toPlainText(),
+            self.price.text(),
+            self.package_volume.text()
+        )
+        
+        if self.add_edit_CheckBox.isChecked():
+            add_data(*data_item_coffee)
+        else:
+            edit_data(self.id_title_LineEdit.text(), *data_item_coffee)
